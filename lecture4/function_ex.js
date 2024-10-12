a6e02534cd72cf7596e5ab98c1c6dea7078b8d42
fs203 <fs2031@icloud.com>
2024-10-12 12:50:02 +0900

    update

--- lecture4/function_ex.js
+++ lecture4/function_ex.js
@@ -1,18 +1,118 @@
-//課題のJavaScriptをひたすら書いて行こう!!
-function getOmikujiFunc() {
-  let rndm = Math.random();
-  return getOmikujiTest(rndm);
-}
+// //課題のJavaScriptをひたすら書いて行こう!!
 
-function getOmikujiTest(rndm) {
-  if (rndm < 0.3) {
-    return "大吉";
-  } else if (rndm >= 0.3 && rndm < 0.7) {
-    return "吉";
-  } else if (rndm >= 0.7 && rndm < 0.95) {
-    return "凶";
-  } else {
-    return "大凶";
-  }
+// 課題１
+
+// function getOmikujiFunc() {
+//   let rndm = Math.random();
+//   return getOmikujiTest(rndm);
+// }
+
+// function getOmikujiTest(rndm) {
+//   if (rndm < 0.3) {
+//     return "大吉";
+//   } else if (rndm >= 0.3 && rndm < 0.7) {
+//     return "吉";
+//   } else if (rndm >= 0.7 && rndm < 0.95) {
+//     return "凶";
+//   } else {
+//     return "大凶";
+//   }
+// }
+// console.log(getOmikujiFunc());
+
+// function getOmikuji() {
+//   let rndm = Math.random();
+//   if (rndm < 0.3) {
+//     return "大吉";
+//   } else if (rndm >= 0.3 && rndm < 0.7) {
+//     return "吉";
+//   } else if (rndm >= 0.7 && rndm < 0.95) {
+//     return "凶";
+//   } else {
+//     return "大凶";
+//   }
+// }
+// console.log(getOmikuji());
+
+// 課題２
+// 関数リテラル
+
+// let getOmikujiTestByLit = function () {
+//   let rndm = Math.random();
+//   if (rndm < 0.3) {
+//     return "大吉";
+//   } else if (rndm >= 0.3 && rndm < 0.7) {
+//     return "吉";
+//   } else if (rndm >= 0.7 && rndm < 0.95) {
+//     return "凶";
+//   } else {
+//     return "大凶";
+//   }
+// };
+// console.log(getOmikujiTestByLit());
+
+// let getOmikujiByLit = function () {
+//   let rndm = Math.random();
+//   if (rndm < 0.3) {
+//     return "大吉";
+//   } else if (rndm >= 0.3 && rndm < 0.7) {
+//     return "吉";
+//   } else if (rndm >= 0.7 && rndm < 0.95) {
+//     return "凶";
+//   } else {
+//     return "大凶";
+//   }
+// };
+// console.log(getOmikujiByLit());
+
+// アロー関数
+// let getOmikujiTestByArr = () => {
+//   let rndm = Math.random();
+//   if (rndm < 0.3) {
+//     return "大吉";
+//   } else if (rndm >= 0.3 && rndm < 0.7) {
+//     return "吉";
+//   } else if (rndm >= 0.7 && rndm < 0.95) {
+//     return "凶";
+//   } else {
+//     return "大凶";
+//   }
+// };
+// console.log(getOmikujiTestByArr());
+
+// let getOmikujiByArr = () => {
+//   let rndm = Math.random();
+//   if (rndm < 0.3) {
+//     return "大吉";
+//   } else if (rndm >= 0.3 && rndm < 0.7) {
+//     return "吉";
+//   } else if (rndm >= 0.7 && rndm < 0.95) {
+//     return "凶";
+//   } else {
+//     return "大凶";
+//   }
+// };
+// console.log(getOmikujiByArr());
+
+// 課題３
+
+// 作業A
+let scope = "ぴかちゅう";
+function viewValue() {
+  let scope = "いーぶい";
+  console.log(scope);
 }
-console.log(getOmikujiFunc());
+viewValue();
+console.log(scope);
+
+// 作業B
+// let scope = "ぴかちゅう";
+// function viewValue() {
+//   scope = "いーぶい";
+//   console.log(scope);
+// }
+// viewValue();
+// console.log(scope);
+
+[説明];
+// "ぴかちゅう"の文字列が入ってる let scope はグローバルスコープで、viewValue関数の"いーぶい"はローカルスコープである。関数内でのconcole.logはローカルスコープが優先されるのでviewValue関数での出力は"いーぶい"となる。viewValue関数外のconsole.logはローカルスコープの影響を受けないのでグローバルスコープの"ぴかちゅう"が出力される。
