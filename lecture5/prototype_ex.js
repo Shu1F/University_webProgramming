88e9cd441b07337a447ebac19156e9faa5ead44a
fs203 <fs2031@icloud.com>
2024-10-22 19:46:00 +0900

    update

--- lecture5/prototype_ex.js
+++ lecture5/prototype_ex.js
@@ -1,15 +1,40 @@
 //課題のJavaScriptをひたすら書いて行こう!!
 // [課題１]
 
-let Omikuji = function () {};
-let rndm = Math.random();
-
-if (rndm < 0.3) {
-  console.log(`大吉 ${rndm}`);
-} else if (rndm >= 0.3 && rndm < 0.7) {
-  console.log(`吉 ${rndm}`);
-} else if (rndm >= 0.7 && rndm < 0.95) {
-  console.log(`凶 ${rndm}`);
-} else {
-  console.log(`大凶 ${rndm}`);
+function Omikuji() {
+  this.rndm = Math.random();
 }
+
+Omikuji.prototype.shake = function () {
+  this.rndm = Math.random();
+};
+
+Omikuji.prototype.getOmikuji = function () {
+  if (rndm < 0.3) {
+    console.log(`大吉 ${this.rndm}`);
+  } else if (rndm >= 0.3 && rndm < 0.7) {
+    console.log(`吉 ${this.rndm}`);
+  } else if (rndm >= 0.7 && rndm < 0.95) {
+    console.log(`凶 ${this.rndm}`);
+  } else {
+    console.log(`大凶 ${this.rndm}`);
+  }
+};
+
+const secOmikuji = new Omikuji();
+console.log(secOmikuji.getOmikuji());
+
+function getOmikuji() {
+  let rndm = Math.random();
+  if (rndm < 0.3) {
+    console.log(`大吉 ${rndm}`);
+  } else if (rndm >= 0.3 && rndm < 0.7) {
+    console.log(`吉 ${rndm}`);
+  } else if (rndm >= 0.7 && rndm < 0.95) {
+    console.log(`凶 ${rndm}`);
+  } else {
+    console.log(`大凶 ${rndm}`);
+  }
+}
+
+console.log(getOmikuji());
