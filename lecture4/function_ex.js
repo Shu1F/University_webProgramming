10c56a390d948e172715bca66f0735dd7adc9950
fs203 <fs2031@icloud.com>
2024-11-03 17:21:39 +0900

    update

--- lecture4/function_ex.js
+++ lecture4/function_ex.js
@@ -6,17 +6,20 @@
 
 // let res = getTriangle(2, 6);
 // console.log(res);
-
-function getOmikujiTest() {
+function getOmikuji() {
   let rndm = Math.random();
+  return getOmikujiTest(rndm);
+}
 
+function getOmikujiTest(rndm) {
   if (rndm < 0.3) {
-    console.log(`大吉 ${rndm}`);
+    return "大吉";
   } else if (rndm >= 0.3 && rndm < 0.7) {
-    console.log(`吉 ${rndm}`);
+    return "吉";
   } else if (rndm >= 0.7 && rndm < 0.95) {
-    console.log(`凶 ${rndm}`);
+    return "凶";
   } else {
-    console.log(`大凶 ${rndm}`);
+    return "大凶";
   }
 }
+console.log(getOmikuji());
