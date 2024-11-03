598a045dad9440143a2adaffe5fccf6a9338e937
fs203 <fs2031@icloud.com>
2024-11-03 22:16:51 +0900

    update

--- lecture4/function_ex.js
+++ lecture4/function_ex.js
@@ -6,12 +6,46 @@
 
 // let res = getTriangle(2, 6);
 // console.log(res);
-function getOmikujiDo() {
-  let rndm = Math.random();
-  return getOmikujiTest(rndm);
-}
 
-function getOmikujiTest(rndm) {
+// <1>
+// function getOmikujiDo() {
+//   let rndm = Math.random();
+//   return getOmikujiTest(rndm);
+// }
+
+// function getOmikujiTest(rndm) {
+//   if (rndm < 0.3) {
+//     return "大吉";
+//   } else if (rndm >= 0.3 && rndm < 0.7) {
+//     return "吉";
+//   } else if (rndm >= 0.7 && rndm < 0.95) {
+//     return "凶";
+//   } else {
+//     return "大凶";
+//   }
+// }
+// console.log(getOmikujiDo());
+
+// // <2>
+// function getOmikuji() {
+//   let rndm = Math.random();
+//   if (rndm < 0.3) {
+//     return "大吉";
+//   } else if (rndm >= 0.3 && rndm < 0.7) {
+//     return "吉";
+//   } else if (rndm >= 0.7 && rndm < 0.95) {
+//     return "凶";
+//   } else {
+//     return "大凶";
+//   }
+// }
+// console.log(getOmikuji());
+
+function getOmikuji() {
+  let rndm = Math.random();
+  return getOmikujiByLit(rndm);
+}
+let getOmikujiByLit = function (rndm) {
   if (rndm < 0.3) {
     return "大吉";
   } else if (rndm >= 0.3 && rndm < 0.7) {
@@ -21,7 +55,4 @@ function getOmikujiTest(rndm) {
   } else {
     return "大凶";
   }
-}
-console.log(getOmikujiDo());
-
-function getOmikuji() {}
+};
